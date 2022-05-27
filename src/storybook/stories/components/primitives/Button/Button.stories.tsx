45c9d5ab410b4b83b19336a34c64e80ddc540809
Vidhi Kataria <GeekyAnts@Vidhi-Kataria-B453-MacBook-Air.local>
2022-05-27 11:16:01 +0530

    fixes

--- src/storybook/stories/components/primitives/Button/Button.stories.tsx
+++ src/storybook/stories/components/primitives/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta } from "@storybook/react";
 import Wrapper from "../../Wrapper";
-import { Button } from "native-base";
+import { Button, Stack, VStack } from "native-base";
 
 export default {
   title: "Components / Forms / Button",
